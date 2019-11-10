11ea037001ba99f811a51d6e5316b09e4588ff7e
shabbir19 <55030692+shabbir19@users.noreply.github.com>
2019-11-10 13:41:15 -0500

    up

--- src/base/Config.java
+++ src/base/Config.java
@@ -2,7 +2,6 @@ package base;
 
 
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
