2a6db4f72a446ef65943157fcd97766c7fe5508b
shabbir19 <55030692+shabbir19@users.noreply.github.com>
2019-11-10 14:09:33 -0500

    up

--- src/base/Config.java
+++ src/base/Config.java
@@ -4,15 +4,13 @@ package base;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-
 import util.Webdriverfunction;
 
 
 
 
 public class Config extends Webdriverfunction {
-	@Parameters("browser")
+	
 	@BeforeMethod(alwaysRun=true)
 	public void browser(){
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\shabbir.DESKTOP-DKE3O53\\Documents\\GitHub\\twitterlogin\\drivers\\chromedriver.exe");
--- src/testing/Login.java
+++ src/testing/Login.java
@@ -1,4 +1,4 @@
-package test;
+package testing;
 
 import org.openqa.selenium.By;
 import org.testng.Assert;
