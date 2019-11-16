4c81f9b758370339b4d862342e9070a19c2e64cc
shabbir19 <55030692+shabbir19@users.noreply.github.com>
2019-11-16 17:16:37 -0500

    up

--- src/values/Twitterval.java
+++ src/values/Twitterval.java
@@ -1,8 +1,8 @@
 package values;
 
 public class Twitterval {
-	public String email_val="shabbir8844@gmail.com";
-	public String pass_val="8844tgsA";
+	public String email_val="shabbir";
+	public String pass_val="twitter";
 	public String invalidpass_val="twitter";
 
 }
