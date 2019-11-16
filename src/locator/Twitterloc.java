9063d306cab3131b6d1f26b5847354102857bfc2
shabbir19 <55030692+shabbir19@users.noreply.github.com>
2019-11-16 17:00:08 -0500

    up

--- src/locator/Twitterloc.java
+++ src/locator/Twitterloc.java
@@ -6,6 +6,7 @@ public class Twitterloc {
 	public String email ="//*[@id=\'page-container\']/div/div[1]/form/fieldset/div[1]/input";
 	public String password ="//*[@id=\'page-container\']/div/div[1]/form/fieldset/div[2]/input";
 	public String login ="//*[@id=\'page-container\']/div/div[1]/form/div[2]/button";
+	public String tittlelogo="//*[@id=\'doc\']/div/div[1]/div[1]/div[2]/div[1]";
 	
 
 }
--- src/testing/Login.java
+++ src/testing/Login.java
@@ -23,16 +23,32 @@ public class Login extends Config{
 			System.out.println("Browser couldnt open twitter page");
 		}
 	}
+//	@Test
+//	public void tittletest() {
+//		String title=driver.getTitle();
+//		if(title.equalsIgnoreCase("https://twitter.com/")){
+//			System.out.println("Browser opened twitter homepage");
+//		}
+//		else{
+//			System.out.println("Browser couldnt open twitter page");
+//		}
+//		
+//	}
 	@Test
-	public void tittletest() {
-		String title=driver.getTitle();
-		if(title.equalsIgnoreCase("https://twitter.com/")){
-			System.out.println("Browser opened twitter homepage");
+	public void LogoVisibilityTest(String Locator) {
+		boolean awq =	driver.findElement(By.xpath(twitloc.tittlelogo)).isDisplayed();
+		if(awq==true) {
+			System.out.println("Twitter logo is Displayed");
 		}
-		else{
-			System.out.println("Browser couldnt open twitter page");
+
+	}
+
+	@Test(dependsOnMethods = {"LogoVisibilityTest"})
+	public void LogoEnableTest(String Locator) {
+		boolean xyz = 	driver.findElement(By.xpath(twitloc.tittlelogo)).isEnabled();
+		if(xyz==true) {
+			System.out.println("Fandango logo is Enabled");
 		}
-		
 	}
 	@Test
 	public void twitterlogin(){
@@ -43,14 +59,14 @@ public class Login extends Config{
 		TypeByXpath(twitloc.password,twitval.pass_val);
 		ClickByXpath(twitloc.login);
 	}
-	@Test
-	public void loginverify(){
-		String Name=driver.findElement(By.xpath("//*[@id=\'react-root\']/div/div/div/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/h2/span")).getText();
-		String actName=Name.trim();
-		System.out.println(actName);
-		Assert.assertEquals(actName, "Home");
-		
-	}
+//	@Test
+//	public void loginverify(){
+//		String Name=driver.findElement(By.xpath("//*[@id=\'react-root\']/div/div/div/main/div/div/div/div/div/div[1]/div[1]/div/div/div/div/div[1]/div/h2/span")).getText();
+//		String actName=Name.trim();
+//		System.out.println(actName);
+//		Assert.assertEquals(actName, "Home");
+//		
+//	}
 	@Test
 	public void loginwthinvalidpass(){
 		System.out.println("target login with invalid password");
